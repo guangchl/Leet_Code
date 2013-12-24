e054084e6d2b4afea8fe519855d73847c5b891cc
Guangcheng Lu <guangchl@andrew.cmu.edu>
2013-12-24 08:24:13 -0500

    12/23/2013 - 8_problems

---
+++ src/leet_code/Problems.java
@@ -0,0 +1,246 @@
+package leet_code;
+
+import java.util.Arrays;
+
+import sun.awt.image.OffScreenImage;
+
+public class Problems {
+	// **************************** HELPER CLASS ****************************
+	/** Definition for binary tree */
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+	
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+
+	// ****************************** SOLUTIONS ******************************
+	/**
+	 * Single Number
+	 * 
+	 * Given an array of integers, every element appears twice except for one.
+	 * Find that single one. Time: O(n). Space: O(0).
+	 */
+	public int singleNumber(int[] A) {
+		// Since A^B^A == B, xor every other element with first one of the A
+		for (int i = 1; i < A.length; i++) {
+			A[0] ^= A[i];
+		}
+		return A[0];
+	}
+
+	/**
+	 * Maximum Depth of Binary Tree
+	 * 
+	 * Given a binary tree, find its maximum depth. The maximum depth is the
+	 * number of nodes along the longest path from the root node down to the
+	 * farthest leaf node.
+	 */
+	public int maxDepth(TreeNode root) {
+		if (root == null) { // base case: empty tree from root
+			return 0;
+		} else { // recursion
+			return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+		}
+	}
+
+	/**
+	 * Same Tree
+	 * 
+	 * Given two binary trees, write a function to check if they are equal or
+	 * not. Two binary trees are considered equal if they are structurally
+	 * identical and the nodes have the same value.
+	 */
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == null && q == null) { // p and q are both empty tree
+			return true;
+		} else if (p == null || q == null) { // one of p and q is empty tree
+			return false;
+		} else { // both p and q are not empty
+			return p.val == q.val && isSameTree(p.left, q.left)
+					&& isSameTree(p.right, q.right);
+		}
+	}
+
+	/**
+	 * Reverse Integer
+	 * 
+	 * Reverse digits of an integer.
+	 */
+	public int reverseInteger(int x) {
+		int result = 0;
+		while (x != 0) {
+			result *= 10;
+			result += x % 10;
+			x /= 10;
+		}
+		return result;
+	}
+
+	/**
+	 * Best Time to Buy and Sell Stock I
+	 * 
+	 * Say you have an array for which the ith element is the price of a given
+	 * stock on day i. If you were only permitted to complete at most one
+	 * transaction (ie, buy one and sell one share of the stock), design an
+	 * algorithm to find the maximum profit.
+	 */
+	public int maxProfit1(int[] prices) {
+		int[]
+	}
+
+	/**
+	 * Best Time to Buy and Sell Stock II
+	 * 
+	 * Say you have an array for which the ith element is the price of a given
+	 * stock on day i. Design an algorithm to find the maximum profit. You may
+	 * complete as many transactions as you like (ie, buy one and sell one share
+	 * of the stock multiple times). However, you may not engage in multiple
+	 * transactions at the same time (ie, you must sell the stock before you buy
+	 * again).
+	 */
+	public int maxProfit2(int[] prices) {
+
+	}
+
+	/**
+	 * Best Time to Buy and Sell Stock III
+	 * 
+	 * Say you have an array for which the ith element is the price of a given
+	 * stock on day i. Design an algorithm to find the maximum profit. You may
+	 * complete at most two transactions.
+	 * 
+	 * Note: You may not engage in multiple transactions at the same time (ie,
+	 * you must sell the stock before you buy again).
+	 */
+	public int maxProfit3(int[] prices) {
+
+	}
+
+	/**
+	 * Unique Binary Search Trees
+	 * 
+	 * Given n, how many structurally unique BST's (binary search trees) that
+	 * store values 1...n? For example: Given n = 3, there are a total of 5
+	 * unique BST's.
+	 */
+	public int numTrees(int n) {
+		// base case
+		if (n == 0 || n == 1) {
+			return 1;
+		}
+		
+		// recursive traverse every possible case
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum = sum + numTrees(i) * numTrees(n - i - 1);
+		}
+		
+		return sum;
+	}
+	
+	/**
+	 * Linked List Cycle
+	 * 
+	 * Given a linked list, determine if it has a cycle in it.
+	 * 
+	 * Follow up: Can you solve it without using extra space?
+	 */
+	public boolean hasCycle(ListNode head) {
+		// set two runners
+		ListNode slow = head;
+		ListNode fast = head;
+		
+		// fast runner move 2 steps at one time while slow runner move 1 step,
+		// if traverse to a null, there must be no loop
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Search Insert Position
+	 * 
+	 * Given a sorted array and a target value, return the index if the target
+	 * is found. If not, return the index where it would be if it were inserted
+	 * in order. You may assume no duplicates in the array.
+	 */
+    public int searchInsert(int[] A, int target) {
+    	// give a range to for recursion
+        return binarySearchInsert(A, target, 0, A.length - 1);
+    }
+    
+    public int binarySearchInsert(int[] A, int target, int left, int right) {
+    	// base case: length of insert range == 1
+    	if (left == right) {
+			if (A[left] >= target) {
+				return left;
+			} else {
+				return left + 1;
+			}
+		}
+    	
+    	// binary recursion
+    	int mid = (left + right) / 2;
+    	if (A[mid] == target) {
+			return mid;
+		} else if (A[mid] < target) {
+			return binarySearchInsert(A, target, mid + 1, right);
+		} else {
+			if (mid == left) { // length of insert range == 2
+				return mid;
+			} else {
+				return binarySearchInsert(A, target, left, mid - 1);
+			}
+		}
+    }
+    
+	/**
+	 * Remove Duplicates from Sorted List
+	 * 
+	 * Given a sorted linked list, delete all duplicates such that each element
+	 * appear only once. For example: Given 1->1->2, return 1->2. Given
+	 * 1->1->2->3->3, return 1->2->3.
+	 */
+	public ListNode deleteDuplicates(ListNode head) {
+		if (head != null) {
+			ListNode iter = head;
+			while (iter.next != null) {
+				if (iter.next.val == iter.val) {
+					iter.next = iter.next.next;
+				} else {
+					iter = iter.next;
+				}
+			}
+		}
+		return head;
+	}
+
+	public void test() {
+		int[] A = { 1, 3 };
+		System.out.println(searchInsert(A, 0));
+	}
+
+	public static void main(String[] args) {
+		Problems m = new Problems();
+		m.test();
+	}
+
+}
